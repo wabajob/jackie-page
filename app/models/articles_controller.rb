e6343fc5137500fecdf8e85a34ef2d570144598c
soroosh shams <soroosh2468@hotmail.com>
2019-12-16 15:23:37 +1100

    add delete and partials

--- app/models/articles_controller.rb
+++ app/models/articles_controller.rb
@@ -1,7 +1,32 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+      flash[:notice] = "Article was succesfully deleted"
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:notice] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def create
