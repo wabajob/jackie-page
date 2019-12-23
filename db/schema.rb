7f6536c5da4eb41bf966fc502b5f284b2218eab9
soroosh shams <soroosh2468@hotmail.com>
2019-12-23 13:22:57 +1100

    add passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_014824) do
+ActiveRecord::Schema.define(version: 2019_12_23_022051) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_014824) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "users"
