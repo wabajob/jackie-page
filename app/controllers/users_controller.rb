f2c86d6598bf734a39090bdd67af774bac11bdd9
soroosh shams <soroosh2468@hotmail.com>
2019-12-23 14:10:24 +1100

    add edit / update and form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,23 @@ class UsersController < ApplicationController
     if @user.save
       #session[:user_id] = @user.id
       flash[:success] = "Welcome to Rooshi Apparel #{@user.username}"
-      redirect_to article_path
+      redirect_to articles_path
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
