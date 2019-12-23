7781732886e53800c13c3333ed4bc620791d8c29
soroosh shams <soroosh2468@hotmail.com>
2019-12-23 11:30:02 +1100

    bootstrap rails 6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "bootstrap"
 
+import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('.example-popover').popover()
+})
 
 
 
