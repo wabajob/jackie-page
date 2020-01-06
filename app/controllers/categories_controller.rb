7db008dcc3084622322249c748675b1fb093d28a
soroosh shams <soroosh2468@hotmail.com>
2020-01-06 15:07:37 +1100

    edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,6 +18,20 @@ def create
   end
 end
 
+def edit
+  @category = Category.find(params[:id])
+end
+
+def update
+   @category = Category.find(params[:id])
+   if @category.update(category_params)
+     flash[:success] = "Category name was successfully updated"
+     redirect_to category_path
+   else
+     render 'edit'
+   end
+ end
+ 
 def show
   @category = Category.find(params[:id])
   @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
